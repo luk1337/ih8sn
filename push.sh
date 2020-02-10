039f93e3534bca69745eee7cfeae20eb358b3f90
LuK1337 <priv.luk@gmail.com>
2020-02-10 23:23:32 +0100

    Add device specific configs

--- push.sh
+++ push.sh
@@ -4,5 +4,12 @@ adb wait-for-device root
 adb wait-for-device remount
 adb wait-for-device push 60-ih8sn.sh /system/addon.d/
 adb wait-for-device push ih8sn /system/bin/
-adb wait-for-device push ih8sn.conf /system/etc/
 adb wait-for-device push ih8sn.rc /system/etc/init/
+
+SERIALNO=$(adb shell getprop ro.boot.serialno)
+
+if [[ -f "ih8sn.conf.${SERIALNO}" ]]; then
+    adb wait-for-device push ih8sn.conf.${SERIALNO} /system/etc/ih8sn.conf
+else
+    adb wait-for-device push ih8sn.conf /system/etc/
+fi
