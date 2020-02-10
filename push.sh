d26fe58ab05bebe5ce5475187b117be8b7deaa79
LuK1337 <priv.luk@gmail.com>
2020-02-10 20:03:25 +0100

    Switch to su domain

--- push.sh
+++ push.sh
@@ -4,4 +4,3 @@ adb wait-for-device root
 adb wait-for-device remount
 adb wait-for-device push ih8sn /system/bin/
 adb wait-for-device push ih8sn.rc /system/etc/init/
-adb wait-for-device shell chcon u:object_r:sysinit_exec:s0 /system/bin/ih8sn
