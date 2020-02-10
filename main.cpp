834264f09bf71b91f791c7cd957c4b04790705ee
LuK1337 <priv.luk@gmail.com>
2020-02-10 23:13:18 +0100

    Hardcode all possible fingerprint props and set default add val to false

--- main.cpp
+++ main.cpp
@@ -5,19 +5,7 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-// copied from build/tools/releasetools/ota_from_target_files.py
-// but with "." at the end, bootimage, and an empty entry
-std::vector<std::string> ro_product_props_default_source_order = {
-    "",
-    "bootimage.",
-    "product.",
-    "product_services.",
-    "odm.",
-    "vendor.",
-    "system.",
-};
-
-void property_override(char const prop[], char const value[], bool add = true) {
+void property_override(char const prop[], char const value[], bool add = false) {
     auto pi = (prop_info *) __system_property_find(prop);
 
     if (pi != nullptr) {
@@ -57,28 +45,33 @@ int main(int argc, char *argv[]) {
     const auto build_description = config.find("BUILD_DESCRIPTION");
     const auto build_security_patch_date = config.find("BUILD_SECURITY_PATCH_DATE");
 
-    for (const auto &source : ro_product_props_default_source_order) {
-        if (build_fingerprint != config.end()) {
-            const auto build_fingerprint_prop_name = "ro." + source + "build.fingerprint";
-            property_override(build_fingerprint_prop_name.c_str(),
-                    build_fingerprint->second.c_str(), false);
+    if (build_fingerprint != config.end()) {
+        for (const auto &prop : {
+            "ro.bootimage.build.fingerprint",
+            "ro.build.fingerprint",
+            "ro.odm.build.fingerprint",
+            "ro.product.build.fingerprint",
+            "ro.system.build.fingerprint",
+            "ro.vendor.build.fingerprint",
+        }) {
+            property_override(prop, build_fingerprint->second.c_str());
         }
     }
 
     if (build_description != config.end()) {
-        property_override("ro.build.description", build_description->second.c_str(), false);
+        property_override("ro.build.description", build_description->second.c_str());
     }
 
     if (build_description != config.end()) {
         property_override("ro.build.version.security_patch",
-                build_security_patch_date->second.c_str(), false);
+                build_security_patch_date->second.c_str());
     }
 
-    property_override("ro.boot.flash.locked", "1", false);
-    property_override("ro.boot.verifiedbootstate", "green", false);
-    property_override("ro.boot.veritymode", "enforcing", false);
-    property_override("ro.boot.warranty_bit", "0", false);
-    property_override("ro.warranty_bit", "0", false);
+    property_override("ro.boot.flash.locked", "1");
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.boot.veritymode", "enforcing");
+    property_override("ro.boot.warranty_bit", "0");
+    property_override("ro.warranty_bit", "0");
 
     return 0;
 }
