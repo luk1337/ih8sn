8ef33e30bba5dcb361acd1c8679b87564ab79f0a
LuK1337 <priv.luk@gmail.com>
2020-02-10 21:29:40 +0100

    Add addon.d script

--- push.sh
+++ push.sh
@@ -2,6 +2,7 @@
 
 adb wait-for-device root
 adb wait-for-device remount
+adb wait-for-device push 60-ih8sn.sh /system/addon.d/
 adb wait-for-device push ih8sn /system/bin/
 adb wait-for-device push ih8sn.conf /system/etc/
 adb wait-for-device push ih8sn.rc /system/etc/init/
