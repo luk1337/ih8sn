932bc4d4c3329401a59c546ad46a0004569f4688
Dan Pasanen <dan.pasanen@gmail.com>
2021-01-26 13:38:58 -0600

    Add support for ro.build.product specific config file

--- push.sh
+++ push.sh
@@ -7,9 +7,12 @@ adb wait-for-device push ih8sn /system/bin/
 adb wait-for-device push ih8sn.rc /system/etc/init/
 
 SERIALNO=$(adb shell getprop ro.boot.serialno)
+PRODUCT=$(adb shell getprop ro.build.product)
 
 if [[ -f "ih8sn.conf.${SERIALNO}" ]]; then
     adb wait-for-device push ih8sn.conf.${SERIALNO} /system/etc/ih8sn.conf
+elif [[ -f "ih8sn.conf.${PRODUCT}" ]]; then
+    adb wait-for-device push ih8sn.conf.${PRODUCT} /system/etc/ih8sn.conf
 else
     adb wait-for-device push ih8sn.conf /system/etc/
 fi
