8b0e83d4a477c1ef3fe9efd3a22870391fb60b67
althafvly <althafvly@gmail.com>
2022-07-05 10:59:09 +0530

    Add support for recovery flashable zip
    - Updater-script based on mindthegapps

--- push.ps1
+++ push.ps1
@@ -14,19 +14,19 @@ if ($use_remount) {
 } else {
     adb wait-for-device shell "stat --format %m /system | xargs mount -o rw,remount"
 }
-adb wait-for-device push 60-ih8sn.sh /system/addon.d/
-adb wait-for-device push ih8sn /system/bin/
-adb wait-for-device push ih8sn.rc /system/etc/init/
+adb wait-for-device push system/addon.d/60-ih8sn.sh /system/addon.d/
+adb wait-for-device push system/bin/ih8sn /system/bin/
+adb wait-for-device push system/etc/init/ih8sn.rc /system/etc/init/
 
 $serialno = adb shell getprop ro.boot.serialno
 $product = adb shell getprop ro.build.product
 
 if (Test-Path "ih8sn.conf.${serialno}" -PathType leaf) {
-    adb wait-for-device push ih8sn.conf.${serialno} /system/etc/ih8sn.conf
+    adb wait-for-device push system/etc/ih8sn.conf.${serialno} /system/etc/ih8sn.conf
 } elseif (Test-Path "ih8sn.conf.${product}" -PathType leaf) {
-    adb wait-for-device push ih8sn.conf.${product} /system/etc/ih8sn.conf
+    adb wait-for-device push system/etc/ih8sn.conf.${product} /system/etc/ih8sn.conf
 } else {
-    adb wait-for-device push ih8sn.conf /system/etc/
+    adb wait-for-device push system/etc/ih8sn.conf /system/etc/
 }
 
 if ($reboot) {
