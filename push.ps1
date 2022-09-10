bc149a7a8afd5eccb76e85b63cb7cfa6f81aba29
althafvly <althafvly@gmail.com>
2022-09-11 00:50:24 +0530

    Check for ro.product.model too
    - For unified variants like alioth/aliothin/aliothcn

--- push.ps1
+++ push.ps1
@@ -25,11 +25,14 @@ adb wait-for-device push system/addon.d/60-ih8sn.sh /system/addon.d/
 adb wait-for-device push system/bin/ih8sn /system/bin/
 adb wait-for-device push system/etc/init/ih8sn.rc /system/etc/init/
 
+$model = adb shell getprop ro.product.model
 $serialno = adb shell getprop ro.boot.serialno
 $product = adb shell getprop ro.build.product
 
 if (Test-Path "system/etc/ih8sn.conf.${serialno}" -PathType leaf) {
     adb wait-for-device push system/etc/ih8sn.conf.${serialno} /system/etc/ih8sn.conf
+} elseif (Test-Path "system/etc/ih8sn.conf.${model}" -PathType leaf) {
+    adb wait-for-device push system/etc/ih8sn.conf.${model} /system/etc/ih8sn.conf
 } elseif (Test-Path "system/etc/ih8sn.conf.${product}" -PathType leaf) {
     adb wait-for-device push system/etc/ih8sn.conf.${product} /system/etc/ih8sn.conf
 } else {
--- push.sh
+++ push.sh
@@ -32,11 +32,14 @@ adb wait-for-device push system/addon.d/60-ih8sn.sh /system/addon.d/
 adb wait-for-device push system/bin/ih8sn /system/bin/
 adb wait-for-device push system/etc/init/ih8sn.rc /system/etc/init/
 
+MODEL=$(adb shell getprop ro.product.model)
 SERIALNO=$(adb shell getprop ro.boot.serialno)
 PRODUCT=$(adb shell getprop ro.build.product)
 
 if [[ -f "system/etc/ih8sn.conf.${SERIALNO}" ]]; then
     adb wait-for-device push system/etc/ih8sn.conf.${SERIALNO} /system/etc/ih8sn.conf
+elif [[ -f "system/etc/ih8sn.conf.${MODEL}" ]]; then
+    adb wait-for-device push system/etc/ih8sn.conf.${MODEL} /system/etc/ih8sn.conf
 elif [[ -f "system/etc/ih8sn.conf.${PRODUCT}" ]]; then
     adb wait-for-device push system/etc/ih8sn.conf.${PRODUCT} /system/etc/ih8sn.conf
 else
